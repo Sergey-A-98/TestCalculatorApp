2896bb6587a8c7e0058bdcee945bf0f73ae0cf59
Сами <serjmercurial@yandex.ru>
2024-06-25 17:23:23 +0500

    Add project files.

---
+++ TestCalculator/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace TestCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("A = ");
+                int a = Int32.Parse(Console.ReadLine());
+                Console.Write("B = ");
+                int b = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Please, select operation 1:+ 2:- 3:* 4:/ 5:exit (enter only numbers) : ");
+                int operation = Int32.Parse(Console.ReadLine());
+                if (operation == 1)
+                {
+                    Console.WriteLine("{0} + {1} = {2}",a,b, a + b);
+                }
+                if (operation == 2)
+                { 
+                    Console.WriteLine("{0} - {1} = {2}", a, b, a - b);
+                }
+                if(operation == 3)
+                {
+                    Console.WriteLine("{0} * {1} = {2}", a, b, a * b);
+                }
+                if (operation == 4)
+                {
+                    Console.WriteLine("{0} / {1} = {2}", a, b, a / b);
+                }
+                if (operation == 5)
+                {
+                    break;
+                }
+
+            }
+          Console.ReadLine();
+
+        }
+    }
+}
